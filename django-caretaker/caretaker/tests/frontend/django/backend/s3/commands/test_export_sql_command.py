b903df348f40f6ca6b2067033604f9c94ac3ee55
Martin Paul Eve <martin@martineve.com>
2022-06-15 21:23:23 +0100

    Fix remaining command test

--- django-caretaker/caretaker/tests/frontend/django/backend/s3/commands/test_export_sql_command.py
+++ django-caretaker/caretaker/tests/frontend/django/backend/s3/commands/test_export_sql_command.py
@@ -1,12 +1,10 @@
-from unittest.mock import patch
-
 import django
-from django.conf import settings
 from moto import mock_s3
 
 from caretaker.management.commands import export_sql
 from caretaker.tests.frontend.django.backend.s3.caretaker_test import \
     AbstractDjangoS3Test
+from caretaker.tests.utils import captured_output
 
 
 @mock_s3
@@ -25,10 +23,11 @@ class TestExportSQLCommand(AbstractDjangoS3Test):
     def test(self):
         self.logger.info('Testing export SQL command test')
 
-        result = export_sql.command.callback(database='',
-                                             frontend_name='Django')
+        with captured_output() as (stdout, stderr):
+            result = export_sql.command.callback(database='',
+                                                 frontend_name='Django')
 
-        self.assertIn('COMMIT', result)
+            self.assertIn('COMMIT', stdout.getvalue())
 
         with self.assertLogs(level='ERROR') as log:
             export_sql.command.callback(database='',
